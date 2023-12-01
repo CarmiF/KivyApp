74192b14abaf295369d5dbf207a6fae7a1cd2b78
Carmi <carmifrank@gmail.com>
2023-12-01 02:47:25 +0200

    Relative view improvments

--- gui/view_recording.py
+++ gui/view_recording.py
@@ -1,12 +1,12 @@
+from kivy.metrics import dp
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.app import App
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 import os
 
-from .utils.utils_main import *
+# from .utils.utils_main import 
 from .audio_text_handaler.play_audio import play_wav
 
 class ViewRecordingPopup(App):
@@ -17,26 +17,32 @@ class ViewRecordingPopup(App):
     
     def build(self):
         main_path = os.path.join(self.definitions.recorded_data_path, str(self.definitions.btn_text))
-        audioPath = os.path.join(main_path ,"audio.wav")
-        textPath = os.path.join(main_path , "text.txt")
-        popup_content = BoxLayout(orientation='vertical', spacing=10, size_hint=(0.8, 0.7))
-        play_audio_btn = Button(text = 'play recording', size_hint_y=None, height=40)
-        play_audio_btn.bind(on_release= lambda _:play_wav(audioPath))
-        file_content =""
-        if os.path.exists(textPath):        
+        audioPath = os.path.join(main_path, "audio.wav")
+        textPath = os.path.join(main_path, "text.txt")
+        
+        # Use relative sizes for layout
+        popup_content = BoxLayout(orientation='vertical')
+        
+        play_audio_btn = Button(text='Play Recording')
+        play_audio_btn.bind(on_release=lambda _: play_wav(audioPath))
+        
+        file_content = ""
+        if os.path.exists(textPath):
             with open(textPath, "r") as file:
                 file_content = str(file.read())
-
+        
         popup_content.add_widget(play_audio_btn)
+        
+        # Use relative sizes for Label
         text_label = Label(
-                text=file_content,
-                size_hint_y=None,
-                height=40,
-                text_size=(400, None),  # Set the width for text wrapping
-                halign='left',
-                valign='top'
-            )
+            text=file_content
+            
+        )
         
         popup_content.add_widget(text_label)
-        self.popup = Popup(title=str(self.definitions.btn_text), content=popup_content, size_hint=(0.8, 0.7))
+        
+        # Create Popup with content
+        self.popup = Popup(title=str(self.definitions.btn_text), content=popup_content, size_hint=(None, None), size=(400, 300), auto_dismiss=True)
         self.popup.open()
+
+# Note: dp() is now imported from kivy.metrics
