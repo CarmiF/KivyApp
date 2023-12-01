10d5881b86be5a04750ba4af721611f2357e4bd0
Carmi <carmifrank@gmail.com>
2023-12-01 05:24:53 +0200

    Improvements in day presentation and selaction

--- gui/analaysis_popup.py
+++ gui/analaysis_popup.py
@@ -16,6 +16,7 @@ from kivy.uix.gridlayout import GridLayout
 import calendar
 import threading
 
+from .utils.utils_main import *
 
 
 class AnalysisPopup():
@@ -25,43 +26,80 @@ class AnalysisPopup():
         self.definitions = definitions
         
     def build(self):
-        layout = GridLayout(cols=7)
         
-        # Dropdown for selecting months
-        month_dropdown = DropDown()
-        for month in calendar.month_name[1:]:
-            btn = Button(text=month, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: month_dropdown.select(btn.text))
-            month_dropdown.add_widget(btn)
+        main_layput = BoxLayout(orientation='vertical')
+        
+        select_month_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.2))
 
-        month_btn = Button(text='Select Month', size_hint=(None, None))
-        month_btn.bind(on_release=month_dropdown.open)
-        month_dropdown.bind(on_select=lambda instance, x: setattr(month_btn, 'text', x))
-        layout.add_widget(month_btn)
+        self.month_dropdown = DropDown()
+        for i in range(1, 13):
+            btn = Button(text=str(i), size_hint_y=None, height=44)
+
+            btn.bind(on_release=lambda btn: self.month_dropdown.select(btn.text))
+            self.month_dropdown.add_widget(btn)
+
+        self.month_btn = Button(text=str(get_current_month()), size_hint=(0.4, 1))
+        self.month_btn.bind(on_release=self.month_dropdown.open)
+        self.month_dropdown.bind(on_select=lambda instance, x: setattr(self.month_btn, 'text', x))
+
+        select_month_layout.add_widget(self.month_btn)
 
         # Dropdown for selecting years
-        year_dropdown = DropDown()
+        self.year_dropdown = DropDown()
         for year in range(2000, 2030):
             btn = Button(text=str(year), size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: year_dropdown.select(btn.text))
-            year_dropdown.add_widget(btn)
+            btn.bind(on_release=lambda btn: self.year_dropdown.select(btn.text))
+            self.year_dropdown.add_widget(btn)
 
-        year_btn = Button(text='Select Year', size_hint=(None, None))
-        year_btn.bind(on_release=year_dropdown.open)
-        year_dropdown.bind(on_select=lambda instance, x: setattr(year_btn, 'text', x))
-        layout.add_widget(year_btn)
+        self.year_btn = Button(text=str(get_current_year()), size_hint=(0.4, 1))
+        self.year_btn.bind(on_release=self.year_dropdown.open)
+        self.year_dropdown.bind(on_select=lambda instance, x: setattr(self.year_btn, 'text', x))
+        
+        go_btn = Button(text = "Go", size_hint=(0.3, 1))
+        go_btn.bind(on_release=lambda instance: self.create_calendar())
 
-        for key in calendar.day_abbr:
-            layout.add_widget(Label(text=key))
+        select_month_layout.add_widget(self.year_btn)
+        select_month_layout.add_widget(go_btn)
 
-        for i in range(1, 32):
-            if i in calendar.monthrange(2023, 1):  # Update the year and month accordingly
-                layout.add_widget(Label(text=str(i)))
-            else:
-                layout.add_widget(Button(text=str(i)))
-
+        # self.create_calendar()
+        self.calendar_layout = GridLayout(cols=7, size_hint=(1, 0.8))
 
 
-        self.popup = Popup(title="Chocse the rec", content=layout, size_hint=(0.8, 0.7))
+        main_layput.add_widget(select_month_layout)
+        main_layput.add_widget(self.calendar_layout)
+
+
+        self.popup = Popup(title="Chocse the rec", content=main_layput, size_hint=(None, None), size=(400, 300), auto_dismiss=True)
         self.popup.open()
         print("analysis")
+
+    def create_calendar(self):
+        
+        self.calendar_layout.clear_widgets()  # Clear existing widgets
+
+        calendar_layout = GridLayout(cols=7, size_hint=(1, 0.8))
+
+        for key in calendar.day_abbr:
+            calendar_layout.add_widget(Label(text=key))
+        
+        weekday, days_in_month = calendar.monthrange(int(self.year_btn.text), int(self.month_btn.text))
+
+        for i in range(0, weekday):
+            calendar_layout.add_widget(Label(text=""))
+        
+        self.data_folder_names = sort_folders_by_date(self.definitions.recorded_data_path)
+        print(self.data_folder_names)
+
+        for i in range(1, days_in_month + 1):
+            # filter_date_strings(self.data_folder_names, i, )
+            
+            
+            calendar_layout.add_widget(Button(text=str(i)))
+        
+
+        self.calendar_layout.add_widget(calendar_layout) 
+        
+
+
+        
+        
